88813f9d54293b3f362b49a80c71d384d4e892fa
Jonathan Eberle <jonathaneberle@Jonathans-MBP.fritz.box>
2022-04-21 16:01:17 +0200

    try fix angular cli

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const rootController = require('./controllers/rootController')
 
 // Config Server
 const app = express()
-const port = 3000
+const port = 3001
 
 // Add Routing
 app.use(ticketRoutes);
